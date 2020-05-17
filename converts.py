a79eb416c0af443397f2ec72dbdb6c331ed3c6f8
Roma Major <65121094+rmmajor@users.noreply.github.com>
2020-05-18 00:13:46 +0300

    Update converts.py

--- converts.py
+++ converts.py
@@ -1,2 +1,16 @@
 import PIL
+import telebot as tb
+from pyes import consts
+
+
+def gen_list_of_formats():
+    res = ""
+    for keys in consts.formats:
+        res += '\n'.join(consts.formats[keys]) + '\n'
+    return res
+
+
+def from_ogg_to_mp3(file_path):
+    pass
+
 
