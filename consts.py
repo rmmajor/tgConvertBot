db3ea01c112a79174518cc2e9e88940f969ad0f9
Roma Major <65121094+rmmajor@users.noreply.github.com>
2020-05-18 00:13:20 +0300

    Update consts.py

--- consts.py
+++ consts.py
@@ -1,1 +1,16 @@
 token = "<your token>"
+
+file_ans = 'We can convert this in one of the following types:'
+
+dir_path = 'tgConvertBot/files/'
+
+content_types = ['voice', 'audio', 'document', 'photo', 'video', 'video_note']
+
+formats = {
+    'audio': ('voice', ),
+    'voice': ('mp3', ),
+    'document': ('txt', 'docx', 'pdf'),
+    'photo': ('png', 'jpg', 'svg'),
+    'video': ('video_note', ),
+    'video_note': ('mp4', )
+}
