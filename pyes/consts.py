e97debf4561b38b0eb83aa50bb06158e77a587d8
Roma Major <65121094+rmmajor@users.noreply.github.com>
2020-05-27 22:21:21 +0300

    Update consts.py

--- pyes/consts.py
+++ pyes/consts.py
@@ -2,16 +2,16 @@ file_ans = 'We can convert this in one of the following types:'
 
 dir_path = 'tgConvertBot/files/'
 
-all_types = {
-    'voice', 'mp3', 'txt', 'docx', 'pdf', 'png', 'jpg', 'svg'
-    'video_note', 'mp4', 'wav', 'ogg', 'mov',
-}
+all_types = (
+    'voice', 'mp3', 'txt', 'docx', 'pdf', 'png', 'jpg', 'ico',
+    'video_note', 'mp4', 'wav', 'ogg', 'mov', 'bmp',
+)
 
 formats = {
     'audio': ('voice', 'ogg', 'wav'),
     'voice': ('mp3', 'ogg', 'wav'),
-    'document': ('txt', 'docx', 'pdf'),
-    'photo': ('png', 'jpg', 'svg'),
+    'text_document': ('txt', 'docx', 'pdf'),
+    'photo': ('png', 'jpg', 'ico', 'bmp'),
     'video': ('video_note', 'avi', 'mov', 'mkv'),
     'video_note': ('mp4', 'mov', 'mkv', 'avi'),
     'animated_sticker': ('sticker without its pack', ),
